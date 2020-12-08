058601618330a18cf49b05d1adaf3e9d7d308d74
socratone <gim2origin@gmail.com>
2020-12-09 05:29:48 +0900

    react router 적용

---
+++ src/components/Routes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Nav from './pc/Nav';
+import Admin from './pc/Admin';
+import Home from  './pc/Home';
+
+const Routes = () => {
+  const isMobile = false;
+
+  if (isMobile) {
+    return (
+      <div>mobile 화면은 아직 지원하지 않습니다.</div>
+    );
+  } else {
+    return ( 
+      <Router>
+        <Nav />
+
+        <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+ 
+export default Routes;
