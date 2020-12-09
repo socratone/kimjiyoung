0f6a89469770df3f2e745d0c8fe4c5e381661661
socratone <gim2origin@gmail.com>
2020-12-10 04:39:07 +0900

    Admin 레이아웃

--- src/components/pc/pages/Admin.js
+++ src/components/pc/pages/Admin.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { activateAdmin } from '../../../store/isAdmin';
+import Button from '../../common/Button';
+import TextInput from '../../common/TextInput';
+import styles from './Admin.module.scss';
 
 const Admin = () => {
   const dispatch = useDispatch();
   const isAdmin = useSelector(state => state.entities.isAdmin);
-
+  console.log('isAdmin:', isAdmin)
   const handleClick = () => {
+    console.log('clicked')
     dispatch(activateAdmin());
   };
-
+  
+  if (isAdmin) {
+    return (
+      <ul className={styles.wrap}>
+        <li>
+          <p className={styles.text}>이미 Admin 계정으로 로그인 됐습니다.</p>
+        </li>
+      </ul>
+    )
+  }
+  
   return ( 
-    <>
-      <div>{`isAdmin의 값은 ${isAdmin.toString()}입니다.`}</div>
-      <button onClick={() => handleClick()}>클릭</button>
-    </> 
+    <ul className={styles.wrap}>
+      <li>
+        <p className={styles.text}>Admin 계정으로 로그인 해주세요.</p>
+      </li>
+      <li>
+        <TextInput width="200px" type="text" />
+      </li>
+      <li>
+        <TextInput width="200px" type="password" />
+      </li>
+      <li>
+        <Button width="100px" onClick={() => handleClick()}>{'로그인'}</Button>
+      </li>
+    </ul> 
   );
 }
  
