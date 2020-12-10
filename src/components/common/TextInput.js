e3d7bb59118e5d48dc8a22b0fc392953a4072c46
socratone <gim2origin@gmail.com>
2020-12-11 07:14:09 +0900

    TextInput prop 추가

--- src/components/common/TextInput.js
+++ src/components/common/TextInput.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './TextInput.module.scss';
 
 const TextInput = props => {
-  const { type, width } = props;
+  const { type, width, value, onChange } = props;
   return (  
     <input 
       type={type} 
       className={styles.textInput}
       style={{ width }}  
+      value={value}
+      onChange={onChange}
     />
   );
 }
