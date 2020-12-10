a506cfcee5aef2ec7cd0df82c9ea9c83a1bdd61e
socratone <gim2origin@gmail.com>
2020-12-11 07:28:20 +0900

    로그인 기능 구현

---
+++ src/api/postSignin.js
@@ -0,0 +1,25 @@
+import { url } from '../config/env';
+
+const headers = new Headers();
+headers.append("Content-Type", "application/json");
+
+const postSignin = async ({ email, password }) => {
+  try {
+    const raw = JSON.stringify({ email, password });
+    const res = await fetch(url + '/api/signin', {
+      method: 'POST',
+      headers,
+      body: raw,
+      redirect: 'follow'
+    });
+
+    const { status, ok } = res;
+    const { message, token } = await res.json();
+    if (token) localStorage.setItem('token', token);
+    return { status, ok, message };
+  } catch (error) {
+    return { status: null, ok: false, message: error };
+  }
+};
+
+export default postSignin;
--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { activateAdmin, disableAdmin } from '../../store/isAdmin';
 import Button from '../common/Button';
 import TextInput from '../common/TextInput';
+import postSignin from '../../api/postSignin';
 import styles from './Admin.module.scss';
 
 const Admin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [info, setInfo] = useState('Admin 계정으로 로그인 해주세요.');
+
   const dispatch = useDispatch();
   const isAdmin = useSelector(state => state.entities.isAdmin);
 
-  const loginAdmin = () => {
-    dispatch(activateAdmin());
+  const handleLogin = async () => {
+    const { status, ok, message } = await postSignin({ email, password });
+    if (ok) dispatch(activateAdmin());
+    setInfo(message);
+    setEmail('');
+    setPassword('');
   };
 
-  const logoutAdmin = () => {
+  const handleLogout = () => {
+    setInfo('Admin 계정으로 로그인 해주세요.');
     dispatch(disableAdmin());
   };
   
@@ -24,25 +34,35 @@ const Admin = () => {
           <p className={styles.text}>Admin 계정으로 로그인 됐습니다.</p>
         </li>
         <li>
-          <Button width="100px" onClick={() => logoutAdmin()}>{'로그아웃'}</Button>
+          <Button width="100px" onClick={() => handleLogout()}>로그아웃</Button>
         </li>
       </ul>
-    )
+    );
   }
   
   return ( 
     <ul className={styles.wrap}>
       <li>
-        <p className={styles.text}>Admin 계정으로 로그인 해주세요.</p>
+        <p className={styles.text}>{info}</p>
       </li>
       <li>
-        <TextInput width="200px" type="text" />
+        <TextInput 
+          width="200px" 
+          type="text" 
+          value={email} 
+          onChange={({ target }) => setEmail(target.value)} 
+        />
       </li>
       <li>
-        <TextInput width="200px" type="password" />
+        <TextInput 
+          width="200px" 
+          type="password" 
+          value={password} 
+          onChange={({ target }) => setPassword(target.value)} 
+        />
       </li>
       <li>
-        <Button width="100px" onClick={() => loginAdmin()}>{'로그인'}</Button>
+        <Button width="100px" onClick={() => handleLogin()}>로그인</Button>
       </li>
     </ul> 
   );
