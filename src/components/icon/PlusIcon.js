7dd7561d8feba1a24d71e46eee573d42e3205d76
socratone <gim2origin@gmail.com>
2020-12-15 10:05:33 +0900

    사용하지 않는 PlusIcon 삭제

--- src/components/icon/PlusIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PlusIcon = ({ size }) => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/></svg>
-  );
-}
- 
-export default PlusIcon;
