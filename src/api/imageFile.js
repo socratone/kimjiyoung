b86f31339963d34486b67ab7c4045eac872ba67f
socratone <gim2origin@gmail.com>
2020-12-18 14:09:52 +0900

    EditMenu에서 삭제 요청시 S3 이미지도 삭제

--- src/api/imageFile.js
+++ src/api/imageFile.js
@@ -56,4 +56,28 @@ const listImageFiles = path => {
   });
 };
 
-export { putImageFile, listImageFiles };
+const deleteImageFile = key => {
+  return new Promise((resolve, reject) => {
+    AWS.config.update({
+      region,
+      accessKeyId,
+      secretAccessKey
+    }); 
+
+    const s3 = new AWS.S3({
+      apiVersion: "2006-03-01",
+      params: { Bucket: albumBucketName }
+    });
+
+    s3.deleteObject({ Key: key }, function(err, data) {
+      if (err) {
+        console.log("There was an error deleting your file: ", err.message);
+        reject({ error: { message: err.message }});
+      }
+      console.log("Successfully deleted file.");
+      resolve({ data });
+    });
+  });
+};
+
+export { putImageFile, listImageFiles, deleteImageFile };
--- src/components/common/EditMenu.js
+++ src/components/common/EditMenu.js
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { removeItem } from '../../store/sacredThings';
 import deleteItem from '../../api/deleteItem';
+import { deleteImageFile } from '../../api/imageFile';
 import YesNoModal from './YesNoModal';
 import styles from './EditMenu.module.scss';
 
 const EditMenu = ({ category, id }) => {
   const dispatch = useDispatch();
   const account = useSelector(state => state.entities.user.account);
+  const sacredThings = useSelector(state => state.entities.sacredThings);
   const [modal, setModal] = useState('');
   const history = useHistory();
 
@@ -23,6 +25,24 @@ const EditMenu = ({ category, id }) => {
     setModal('remove');
   };
 
+  const requestRemoveItem = async () => {
+    const result = await deleteItem(id);
+    if (result.error) return alert(result.error.message);
+    const [item] = sacredThings[category].items.filter(item => item.id === id);
+    console.log('key:', `sacred-things/${category}/${item.mainImage}`)
+    const result2 = await deleteImageFile(`sacred-things/${category}/${item.mainImage}`);
+    if (result2.error) return alert(result.error.message);
+    dispatch(removeItem({ category, id }));
+  };
+
+  const showRemoveModal = () => (
+    <YesNoModal 
+      text="정말로 삭제하시겠습니까?"
+      yes={() => requestRemoveItem()} 
+      no={() => setModal('')} 
+    />
+  );
+
   // if (account === 'admin') {
     return ( 
       <>
@@ -35,15 +55,7 @@ const EditMenu = ({ category, id }) => {
           </p>
         </div> 
 
-        {modal === 'remove' && <YesNoModal 
-          text="정말로 삭제하시겠습니까?"
-          yes={async () => {
-            dispatch(removeItem({ category, id }));
-            const result = await deleteItem(id);
-            if (result.error) return alert(result.error.message);
-          }} 
-          no={() => setModal('')} 
-        />}
+        {modal === 'remove' && showRemoveModal()}
       </>
     );
   // }
