149cd1fec81e887cc17ce2e5878c6b7d759b945e
socratone <gim2origin@gmail.com>
2020-12-20 12:47:40 +0900

    코드 정리

--- src/validate/validateSignup.js
+++ src/validate/validateSignup.js
@@ -5,19 +5,12 @@ const isValidEmail = email => {
 }
 
 const validateSignup = ({ email, password, password2, name }) => {
-  if (email.length < 1) {
-    throw new Error('이메일을 입력해주세요.');
-  } else if (!isValidEmail(email)) {
-    throw new Error('올바른 이메일을 입력하세요.');
-  } else if (password.length < 1 || password2.length < 1) {
-    throw new Error('비밀번호를 입력해주세요.');
-  } else if (password.length < 8) {
-    throw new Error('비밀번호는 8자 이상이어야 합니다.');
-  } else if (password !== password2) {
-    throw new Error('비밀번호가 일치하지 않습니다.');
-  } else if (name.length < 1) {
-    throw new Error('이름을 입력해주세요.');
-  }
+  if (email.length < 1) throw new Error('이메일을 입력해주세요.');
+  if (!isValidEmail(email)) throw new Error('올바른 이메일을 입력하세요.');
+  if (password.length < 1) throw new Error('비밀번호를 입력해주세요.');
+  if (password.length < 8) throw new Error('비밀번호는 8자 이상이어야 합니다.');
+  if (password !== password2) throw new Error('비밀번호가 일치하지 않습니다.');
+  if (name.length < 1) throw new Error('이름을 입력해주세요.');
 };
 
 export default validateSignup;
